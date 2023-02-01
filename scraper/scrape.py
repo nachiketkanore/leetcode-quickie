e382bf8a79fd28d77b5d63955a8c70b928a39e47
Nachiket Kanore <nachiket.kanore@gmail.com>
2023-02-01 21:15:03 +0530

    removed unnecessary headers from request

--- scraper/scrape.py
+++ scraper/scrape.py
@@ -17,18 +17,15 @@ def get_problem_data(title_slug = 'odd-even-jump'):
     with open('.env', 'r') as file:
         cookie = file.read().rstrip()
 
+    # TODO: remove unnnecessary headers
     headers = {
-      'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/109.0',
       'Accept': '*/*',
       'Accept-Language': 'en-US,en;q=0.5',
       'Accept-Encoding': 'gzip, deflate, br',
       'content-type': 'application/json',
-      'x-csrftoken': 'xsfGoCtyOHoWgEQmDrOgse2GiJ0mqT1GzFsFH92va9nosWWXQBhj0usEn5qvRz35',
       'authorization': '',
-      'random-uuid': '972642fe-0975-f04e-3706-b839c65b5962',
       'Origin': 'https://leetcode.com',
       'Connection': 'keep-alive',
-      # 'Referer': 'https://leetcode.com/problems/odd-even-jump/',
       'Cookie': cookie,
       'Sec-Fetch-Dest': 'empty',
       'Sec-Fetch-Mode': 'cors',
@@ -39,6 +36,8 @@ def get_problem_data(title_slug = 'odd-even-jump'):
     response = requests.request("POST", url, headers=headers, data=payload)
 
     return response.text
+
+print(get_problem_data('odd-even-jump'))
 
 # TODO: iterate over all title slugs and scrape the data
 # for idx, title_slug in enumerate(['odd-even-jump', 'split-array-with-same-average']):
